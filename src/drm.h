03104587c3793b7b1e9449c2ea438cc4a52046a3
HurricaneZerox <hurricanezeroxdev@gmail.com>
2026-02-03 17:25:54 +1100

    Changed device to card1

--- src/drm.h
+++ src/drm.h
@@ -8,7 +8,7 @@
 namespace drm {
 
     inline int open_graphics_device() {
-        const char* device_path = "/dev/dri/card0";
+        const char* device_path = "/dev/dri/card1";
         int fd = open(device_path, O_RDWR);
 
         if (fd < 0) {
